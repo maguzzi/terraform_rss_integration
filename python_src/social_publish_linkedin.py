5b1adc0e960ef98fd72aa94c244b90cc63437724
Marco Aguzzi <marco.aguzzi@gmail.com>
2025-04-25 22:17:42 +0200

    stripping down html tags

--- python_src/social_publish_linkedin.py
+++ python_src/social_publish_linkedin.py
@@ -2,6 +2,8 @@ import requests
 import os
 import json
 import linkedin_media_share_manager
+from bs4 import BeautifulSoup
+from logger import logger
 
 max_commentary_length=3000
 
@@ -11,11 +13,25 @@ headers = {
   }
 
 def publish_to_profile(processed_post,profile_id):
-  asset_urn = linkedin_media_share_manager.preare_media_for_post(processed_post,profile_id)
-  publish(processed_post,to_profile_payload(processed_post,profile_id,asset_urn))
+  #asset_urn = linkedin_media_share_manager.preare_media_for_post(processed_post,profile_id)
+  publish(processed_post,to_profile_payload(processed_post,profile_id,'asset_urn'))
 
-       
+def remove_html_tags_with_newlines(html_content):
+
+    soup = BeautifulSoup(html_content, 'html.parser')
+
+    # Replace heading tags (h1 to h6) with newlines
+    for tag in soup.find_all(['h1', 'h2', 'h3', 'h4', 'h5', 'h6','br']):
+        tag.replace_with(tag.get_text() + '\n')
+
+    for tag in soup.find_all(['li']):
+        tag.replace_with(f"- {tag.get_text()}\n")
+
+    return soup.get_text(separator='').strip()
+
 def to_profile_payload(processed_post,profile_id,media_urn):
+   
+  text = remove_html_tags_with_newlines(processed_post.get("body","no body"))
 
   return {
     "author": f"urn:li:person:{profile_id}",
@@ -23,7 +39,7 @@ def to_profile_payload(processed_post,profile_id,media_urn):
     "specificContent": {
         "com.linkedin.ugc.ShareContent": {
             "shareCommentary": {
-                "text": f"{processed_post.get("body","no body")} {processed_post.get("link","no link")}"[:max_commentary_length]
+                "text": f"{text} {processed_post.get("link","no link")}"[:max_commentary_length-1]
             },
             "shareMediaCategory": "IMAGE",
             "media": [
@@ -52,12 +68,14 @@ def publish(processed_post, json_payload):
   
   publish_url = "https://api.linkedin.com/v2/ugcPosts"
   
-  response = requests.post(publish_url, headers=headers, json=json_payload)
+  logger.info(json_payload)
 
-  if response.status_code == 201:
-      print("Successfully posted to LinkedIn!")
-      print(response.json())
-  else:
-      print(f"Failed to post to LinkedIn. Status code: {response.status_code}")
-      print(response.text)
+  #response = requests.post(publish_url, headers=headers, json=json_payload)
+
+  #if response.status_code == 201:
+  #    logger.info("Successfully posted to LinkedIn!")
+  #    logger.debug(response.json())
+  #else:
+  #    logger.error(f"Failed to post to LinkedIn. Status code: {response.status_code}")
+  #   logger.error(response.text)
 
